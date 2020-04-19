57c2c2c1a1aab247c45fac765547a77c009301eb
Leslie <lizhonglin_melody@yeah.net>
2020-04-19 13:16:47 +0800

    add lnmp environment

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Docker
-Docker学习记录
+# Dockerfile & Environment
+RECORD?
+1.added lnmp environment(no optimization) 2020.4.19
---
+++ lnmp/sites/index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "mysql";
+$username = "leslie";
+$password = "123456";
+ 
+try {
+    $conn = new PDO("mysql:host=$servername;", $username, $password);
+    echo "连接成功"; 
+}
+catch(PDOException $e)
+{
+    echo $e->getMessage();
+}
+?>
